be7938842a41545e579b1e607bbf97f009bf678f
HUANG JING CHAO <91250467+rafa920613@users.noreply.github.com>
2022-05-20 21:41:14 +0800

    Update food.js

--- food/food.js
+++ food/food.js
@@ -14,13 +14,19 @@ function draw() {
         rect(50, 100, 150, 2);
     if (choice == 2)
         rect(50, 150, 150, 2);
+    if (choice == 3)
+        rect(50, 200, 150, 2);
+    if (choice == 4)
+        rect(50, 250, 150, 2);
     text("中餐吃水餃", 50, 50);
     text("中餐吃uber eat", 50, 100);
     text("中餐吃food panda", 50, 150);
+    text("中餐吃牛排"50,200);
+    text("中餐吃大餐",50,250);
 }
 
 function mousePressed() {
-    choice = int(random(3));
+    choice = int(random(5));
 }
 
 function initializeFields() {
